8a41092c2e517bb3142bf6f440f5b0bf09e7f7c2
lgbaixauli <lgbaixauli@gmail.com>
2023-01-29 22:14:20 +0100

    [202301292214]

--- pybot/config.py
+++ pybot/config.py
@@ -15,9 +15,35 @@ class Config (Storage):
         super().__init__(filename, "")
         self.read_yaml()
 
-    def get(self, param_name: str = "", default_value: str = "") -> any:
+
+    def get(self, param_name: str = "", default_value: any = None) -> any:
+
+        error = False
+
+        if param_name.find(".") > 0:
+            
+            local_config = self._storage
+
+            for item in param_name.split("."):
+                if item in local_config:
+                    local_config = local_config[item]
+                else:
+                    error = True
+                    output = default_value
+            
+            if not error: 
+                output = local_config
+
+        else:
+            output = self._storage[param_name] if param_name in self._storage else default_value
+
+        return output
+
+
+    def exist (self, param_name: str = "") -> bool:
 
         found = False
+        error = False
 
         if param_name.find(".") > 0:
             local_config = self._storage
@@ -26,14 +52,13 @@ class Config (Storage):
                 if item in local_config:
                     local_config = local_config[item]
                 else:
-                    found = True
-                    output = default_value
+                    error = True
 
-            if not found: 
-                output = local_config
+            if not error: 
+                found = True
 
         else:
-            output = self._storage[param_name] if param_name in self._storage else default_value
+            found = True if param_name in self._storage else False
 
-        return output
-    
+        return found
+
